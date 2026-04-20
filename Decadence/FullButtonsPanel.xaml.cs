33a7c1eb15e87371268b067840bcffe1661114f4
ponte <maximmd@outlook.com>
2026-04-20 22:22:22 +0700

    Добавил UserControl для списка треков

---
+++ Decadence/FullButtonsPanel.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Decadence.Models;
+using System;
+
+namespace Decadence
+{
+    public sealed partial class FullButtonsPanel : UserControl
+    {
+        public bool IsVisible => RootGrid.Visibility == Visibility.Visible;
+        // Событие при клике на трек
+        public event ItemClickEventHandler TrackClicked;
+        public event EventHandler BackClicked;
+
+        public FullButtonsPanel()
+        {
+            this.InitializeComponent();
+            TracksListView.ItemClick += TracksListView_ItemClick;
+        }
+
+        // Установить список треков
+        public void SetTracks(ObservableCollection<TrackItem> tracks)
+        {
+            TracksListView.ItemsSource = tracks;
+            TracksListView.ItemClick += TracksListView_ItemClick;
+        }
+
+        // Показать панель
+        public void Show()
+        {
+            RootGrid.Visibility = Visibility.Visible;
+        }
+
+        // Скрыть панель
+        public void Hide()
+        {
+            RootGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void TracksListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            TrackClicked?.Invoke(this, e);
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            BackClicked?.Invoke(this, EventArgs.Empty);  // ← уведомляем MainPage
+        }
+    }
+}
